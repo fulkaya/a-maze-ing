78e6ae0444ad73b765a02bfef1ca451cd858cb6c
Fulya Dilara Kaya <kayafulyadilara@gmail.com>
2026-09-13 19:42:30 +0300

    break_walls

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -3,4 +3,7 @@ from mazegen import Maze
 
 if __name__ == "__main__":
     grid = Maze(5, 5)
+    grid.break_wall()
+    grid.display()
+    grid.remove_dead_ends()
     grid.display()
--- mazegen/maze.py
+++ mazegen/maze.py
@@ -1,10 +1,14 @@
+import random
+
+
 class Cell():
 
-    def __init__(self, north, east, south, west):
+    def __init__(self, north: bool, east: bool, south: bool, west: bool):
         self.north = north
         self.east = east
         self.south = south
         self.west = west
+        self.is_visited = False
 
     def sum(self):
         return self.north + self.east + self.south + self.west
@@ -13,8 +17,8 @@ class Cell():
 class Maze():
 
     def __init__(self, width: int, height: int):
-        self.width = width
-        self.height = height
+        self.width: int = width
+        self.height: int = height
         self.grid: list[list[Cell]] = self.create_grid()
 
     def create_grid(self):
@@ -33,9 +37,12 @@ class Maze():
             if row:
                 first_cell = row[0]
                 for cell in row:
-                    if cell.north == 1 and cell is first_cell:
+                    if cell is first_cell:
                         print("+", end="")
-                    print("---+", end="")
+                    if cell.north == 1:
+                        print("---+", end="")
+                    else:
+                        print("   +", end="")
                 print()
 
                 for cell in row:
@@ -43,6 +50,8 @@ class Maze():
                         print("|", end="")
                     if cell.east == 1:
                         print("   |", end="")
+                    else:
+                        print("    ", end="")
                 print()
 
         last_cell = self.grid[-1]
@@ -51,3 +60,115 @@ class Maze():
                 print("+", end="")
             print("---+", end="")
         print()
+
+    def break_wall(self):
+        current_pos: tuple[int, int] = (0, 0)
+
+        new_list: list[tuple[int, int]] = [current_pos]
+
+        while new_list:
+            way_list: list[tuple[int, int]] = []
+            x, y = current_pos
+
+            current_pos_north: tuple = x, y - 1
+            current_pos_south: tuple = x, y + 1
+            current_pos_west: tuple = x - 1, y
+            current_pos_east: tuple = x + 1, y
+
+            self.grid[y][x].is_visited = True
+
+            if (
+                0 <= current_pos_north[0] < self.width and
+                0 <= current_pos_north[1] < self.height and not
+                self.grid[current_pos_north[1]][current_pos_north[0]]
+                .is_visited
+            ):
+                way_list.append(current_pos_north)
+
+            if (
+                0 <= current_pos_south[0] < self.width and
+                0 <= current_pos_south[1] < self.height and not
+                self.grid[current_pos_south[1]][current_pos_south[0]]
+                .is_visited
+            ):
+                way_list.append(current_pos_south)
+
+            if (
+                0 <= current_pos_west[0] < self.width and
+                0 <= current_pos_west[1] < self.height and
+                not self.grid[current_pos_west[1]][current_pos_west[0]]
+                .is_visited
+            ):
+                way_list.append(current_pos_west)
+
+            if (
+                0 <= current_pos_east[0] < self.width and
+                0 <= current_pos_east[1] < self.height and not
+                self.grid[current_pos_east[1]][current_pos_east[0]]
+                .is_visited
+            ):
+                way_list.append(current_pos_east)
+
+            if way_list:
+                selected_pos = random.choice(way_list)
+                nx, ny = selected_pos
+                if selected_pos == current_pos_north:
+                    self.grid[y][x].north = 0
+                    self.grid[ny][nx].south = 0
+                if selected_pos == current_pos_south:
+                    self.grid[y][x].south = 0
+                    self.grid[ny][nx].north = 0
+                if selected_pos == current_pos_west:
+                    self.grid[y][x].west = 0
+                    self.grid[ny][nx].east = 0
+                if selected_pos == current_pos_east:
+                    self.grid[y][x].east = 0
+                    self.grid[ny][nx].west = 0
+                current_pos = selected_pos
+                new_list.append(current_pos)
+            else:
+                new_list.pop()
+                if new_list:
+                    current_pos = new_list[-1]
+
+    def remove_dead_ends(self):
+        while True:
+            changed = False
+
+            for y in range(self.height):
+                for x in range(self.width):
+                    way_list: list[str] = []
+                    if self.grid[y][x].sum() == 3:
+
+                        if x - 1 >= 0 and self.grid[y][x].west == 1:
+                            way_list.append('W')
+
+                        if x + 1 < self.width and self.grid[y][x].east == 1:
+                            way_list.append('E')
+
+                        if y - 1 >= 0 and self.grid[y][x].north == 1:
+                            way_list.append('N')
+
+                        if y + 1 < self.height and self.grid[y][x].south == 1:
+                            way_list.append('S')
+
+                        if way_list:
+                            selected_way = random.choice(way_list)
+
+                            if selected_way == 'W':
+                                self.grid[y][x].west = 0
+                                self.grid[y][x - 1].east = 0
+                            if selected_way == 'E':
+                                self.grid[y][x].east = 0
+                                self.grid[y][x + 1].west = 0
+                            if selected_way == 'N':
+                                self.grid[y][x].north = 0
+                                self.grid[y - 1][x].south = 0
+                            if selected_way == 'S':
+                                self.grid[y][x].south = 0
+                                self.grid[y + 1][x].north = 0
+
+                            changed = True
+
+            if not changed:
+                break
